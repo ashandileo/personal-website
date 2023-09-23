93934320e0185df7583f6b345ce4323febb7adf9
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2023-09-23 17:39:35 +0700

    update site meta data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'Personal Website',
+  author: 'Ashandi Leonadi',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
