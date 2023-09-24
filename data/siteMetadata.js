2211d067551f9604be043209454ae19f30166ff8
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2023-09-24 17:05:00 +0700

    change site meta data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: "Ashandi's Website",
+  title: 'Ashandi Leonadi',
   author: 'Ashandi Leonadi',
   headerTitle: '@ashandi.leo',
   description: 'A blog created with Next.js and Tailwind.css',
